f33fa5bd83be73cf4e1851491e08902a3790dcb6
Roger Agostinho <rogeragostinho76@gmail.com>
2025-03-01 00:12:19 +0100

    Create FuncionarioService.java

---
+++ src/main/java/rogerhugo/funcionarioscrud/model/service/FuncionarioService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rogerhugo.funcionarioscrud.model.service;
+
+/**
+ *
+ * @author Róger Hugo
+ */
+public class FuncionarioService {
+    
+}
